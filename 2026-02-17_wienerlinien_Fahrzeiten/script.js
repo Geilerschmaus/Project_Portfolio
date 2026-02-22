32dff61b546a3f78cd29955642c7c297c7eb5177
Roger Avdiyskiy <AVD240096@spengergasse.at>
2026-02-22 18:40:26 +0100

    most basic version underground forecast

--- 2026-02-17_wienerlinien_Fahrzeiten/script.js
+++ 2026-02-17_wienerlinien_Fahrzeiten/script.js
@@ -1,6 +1,10 @@
-import axios from 'https://cdn.skypack.dev/axios';
+import axios from 'https://esm.sh/axios@1.7.4';
 
-const rbls = [4415, 4422];
+let name = [];
+
+//Margaretengürttel U4 heiligen,hüttel gumpendorferstraße U6 sieben,florids 
+
+const rbls = [4415, 4422,4611,4618];
 const url = `https://www.wienerlinien.at/ogd_realtime/monitor?rbl=${rbls.join('&rbl=')}`;
 const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`;
 
@@ -9,13 +13,70 @@ async function getDepartures() {
     const response = await axios.get(proxyUrl);
     
     if (response.data && response.data.data) {
-        const station = response.data.data.monitors[0].locationStop.properties.title;
-        console.log("3. Station gefunden:", station);
-        alert("Station: " + station);
+
+        const data = response.data.data;
+        return data;
+
     } else {
         console.error("Datenstruktur der Wiener Linien unvollständig", response.data);
     }
 
 }
 
-getDepartures();
+function splitDataAndDisplay(data) {
+
+    const tableData = document.getElementById("departures-body")
+    
+    
+    data.monitors.forEach((station,index) => {
+        
+        const line = station.lines[0];
+        
+        if (!line.departures || !line.departures.departure) {
+            console.log("No departures for station:", station.locationStop.properties.title);
+            return;
+        }
+
+        name[index] = {
+            name: station.locationStop.properties.title,
+            departureNext: line.departures.departure[0].departureTime.countdown,
+            departureAfterNext: line.departures.departure[1].departureTime.countdown,
+            direction: line.towards,
+            trafficjam: line.trafficjam,
+            realtime: line.realtimeSupported
+        }
+
+        const newRow = document.createElement("tr");
+        const stationName = document.createElement("td");
+        const departureNext = document.createElement("td");
+        const departureAfterNext = document.createElement("td");
+        const direction = document.createElement("td");
+        const trafficjam = document.createElement("td");
+        const realtime = document.createElement("td");
+        
+        stationName.textContent = name[index].name;
+        departureNext.textContent = name[index].departureNext;
+        departureAfterNext.textContent = name[index].departureAfterNext;
+        direction.textContent = name[index].direction;
+        trafficjam.textContent = name[index].trafficjam ? "Yes" : "No";
+        realtime.textContent = name[index].realtime ? "Yes" : "No";
+
+
+        tableData.appendChild(newRow);
+        newRow.appendChild(stationName);
+        newRow.appendChild(direction);
+        newRow.appendChild(departureNext);
+        newRow.appendChild(departureAfterNext);
+        newRow.appendChild(trafficjam);
+        newRow.appendChild(realtime);
+
+        console.log(index, name[index]);        
+    });
+}
+
+async function main() {
+    const data = await getDepartures();
+    splitDataAndDisplay(data);
+}
+
+main();
