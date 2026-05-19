c1a294248d7cb1f8b47f8b12803007cb62b62f2d
Roger Avdiyskiy <AVD240096@spengergasse.at>
2026-05-19 10:59:25 +0200

    first view on react

---
+++ 2026-05-18_React_Learning/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WelcomeBar from './components/welcome_bar.jsx'
+
+function App() {
+  return (
+    <WelcomeBar />
+  )
+}
+
+export default App
---
+++ 2026-05-18_React_Learning/src/components/welcome_bar.jsx
@@ -0,0 +1,17 @@
+function WelcomeBar() {
+
+    const user = "student";
+
+    return(
+    <div>
+        <h1>
+            Welcome {user} !
+        </h1>
+        <p>
+            this is your first react code
+        </p>
+    </div>
+    )
+}
+
+export default WelcomeBar;
