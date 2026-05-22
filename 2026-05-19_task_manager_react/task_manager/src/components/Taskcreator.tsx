767868ed7c2af4a5c12dd38410ec823af15d83a5
Roger Avdiyskiy <AVD240096@spengergasse.at>
2026-05-22 11:03:33 +0200

    wip

--- 2026-05-19_task_manager_react/task_manager/src/components/Taskcreator.tsx
+++ 2026-05-19_task_manager_react/task_manager/src/components/Taskcreator.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 
-function TaskInput() {
-  // 1. THE STORAGE (State)
-  const [inputValue, setInputValue] = useState<string>("");
-  // 2. THE BRIDGE (The Event Handler)
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const textInsideInput = event.target.value;
-    setInputValue(textInsideInput); 
-  };
-  // 3. THE MIRROR (The JSX)
+function Taskcreator() {
+
+  const [task, setTask] = useState<string>('');
+
+
+
   return (
-    <div className="task-input-container">
-      <input 
-        type="text" 
-        value={inputValue}        
-        onChange={handleInputChange} 
-      />
-      <button onClick={() => console.log(inputValue)}>Add Task</button>
+    <div>
+      <input type="text"
+       value={task}
+       onChange={(task) => setTask(task.target.value)}
+       />
+       <button onClick={}></button>
+      <p>{task}</p>
     </div>
-  );
+  )
+
 }
-export default TaskInput;
+
+export default Taskcreator;
