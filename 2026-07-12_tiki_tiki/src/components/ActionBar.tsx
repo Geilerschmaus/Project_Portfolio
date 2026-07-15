e46ec0f9ecfa4a315c446848a7a001007bc6b2c0
Roger Avdiyskiy <AVD240096@spengergasse.at>
2026-07-15 12:46:52 +0200

    short ting wip

--- 2026-07-12_tiki_tiki/src/components/ActionBar.tsx
+++ 2026-07-12_tiki_tiki/src/components/ActionBar.tsx
@@ -1,23 +1,59 @@
 import type { Video } from "../types.ts";
+import {Heart,Bookmark,Share2,MessageCircle,PlusCircleIcon} from 'lucide-react'
 
-interface VideoInfo {
+interface ActionBarProps {
 
     video: Video
+    onLike: (videoId: string) => void;
+    onBookmark: (videoId: string) => void;
+    onFollow: (username: string) => void;
 }
 
-function ActionBar({video}: VideoInfo){
+function ActionBar({video, onLike, onBookmark, onFollow}: ActionBarProps){
 
 
 
     return(
         <div>
 
-            <button type="button"></button>
-            <p>{video.likeAmount}</p>
-            <button type="button"></button>
-            <p>{video.bookmarksAmount}</p>
-            <button type="button"></button>
-            <p>{video.shareAmount}</p>
+            <div>
+                <img src={video.avatarUrl} alt="Profile-Picture" />
+                <button type="button" className="follow-button" onClick={() => onFollow(video.username)}>
+                    <PlusCircleIcon size={10} color="black" fill=""></PlusCircleIcon>
+                </button>
+            </div>
+
+            <div>
+
+                <button type="button" className="clean-actionBar-button" onClick={() => {onLike(video.id)}}>
+
+                <Heart size={28} color="black" fill={video.isLiked ? "red" : "white"}></Heart>
+                </button>
+
+                <p>{video.likeAmount}</p>
+            </div>
+
+            <div>
+
+                <button type="button" className="clean-actionBar-button" onClick={() => {onBookmark(video.id)}}>
+
+                <Bookmark size={28} color="black" fill={video.isBookmarked ? "yellow" : "white"}></Bookmark>
+                </button>
+                <p>{video.bookmarksAmount}</p>
+            </div>
+
+            <div>
+
+                <MessageCircle size={28} color="black" fill="white"></MessageCircle>
+                <p>{video.shareAmount}</p>
+            </div>
+
+            <div>
+
+                <Share2 size={28} color="black" fill="white"></Share2>
+                <p>{video.shareAmount}</p>
+            </div>
+
         </div>
     )
 
